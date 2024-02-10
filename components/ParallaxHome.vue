bc2b04ae35b2bbc5233984572ca9c70ad27e9f38
unknown <jose_millan_9@hotmail.com>
2024-02-09 21:13:26 -0400

    feb2024

---
+++ components/ParallaxHome.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-parallax
+      :src="merchant.banner"
+  >
+    <v-col class="text-center">
+      <v-img 
+        width="180"
+        class="mx-auto"
+        :src="merchant.logo">
+      </v-img>
+      <h2>{{ merchant.slogan }}</h2>
+      <h3>Ahora puedes pedir tu delivery cuando quieras!</h3>
+    </v-col>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  computed: {
+    merchant() {
+      return this.$store.state.merchant
+    }
+  },
+}
+</script>
+
+<style scoped></style>
